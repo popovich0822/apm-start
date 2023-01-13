362b51d270b89520d8864e072a03d9f0e16c1cbe
bruce <120096@o365.tku.edu.tw>
2023-01-13 14:21:32 +0800

    passing parameters to a route & handing null and undefined

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './IProduct';
 
 @Component({  
   templateUrl: './product-detail.component.html',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
 
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': '',
+      'releaseDate': '',
+      'description': '',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    };
+  }
 }
