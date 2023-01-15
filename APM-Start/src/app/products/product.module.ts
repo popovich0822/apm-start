d856f2e816b4e5b8ec974d58450f222140751b96
bruce <120096@o365.tku.edu.tw>
2023-01-15 15:41:38 +0800

    shared modules

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe    
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { 
@@ -25,7 +20,8 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
       }
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
